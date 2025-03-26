532a6fa4b5eecfeb523c95d4241d655f55c74f40
Otso Saarinen <saarinenotso@gmail.com>
2025-03-26 16:53:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import {
     AlertDialog,
     AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
     AlertDialogDescription,
     AlertDialogFooter,
