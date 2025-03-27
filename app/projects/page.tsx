62be5b2b36dfae891a87e1d7ff678175091d3fd9
otsosaarinen <saarinenotso@gmail.com>
2025-03-27 17:17:41 +0200

    add description to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,6 +13,7 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 
 type ProjectItem = {
+    Title: string;
     Description: string;
     GitHub: string;
     Badges: string[];
@@ -25,6 +26,7 @@ type ProjectType = {
 const projects: ProjectType = {
     Klemmari: [
         {
+            Title: "AI assistant for web application",
             Description:
                 "A web application featuring a GPT-4o-powered chatbot that assists users with settings adjustments, navigation, and answering questions. The language model is built using a RAG (Retrieval-Augmented Generation) model for optimal responses and is controlled through Microsoft Azure.",
             GitHub: "https://github.com/otsosaarinen/klemmari",
@@ -40,6 +42,7 @@ const projects: ProjectType = {
     ],
     Thesis: [
         {
+            Title: "Real-time patient data collection and analysis using the Apache Kafka platform",
             Description:
                 "My thesis focuses on real-time patient data collection and analysis using the Apache Kafka platform. As part of the research, I developed a prototype for healthcare that integrates Apache Kafka, Python, InfluxDB, and Grafana to enable real-time health data visualization.",
             GitHub: "https://github.com/otsosaarinen/thesis",
@@ -54,12 +57,14 @@ const projects: ProjectType = {
     ],
     "Spotify Web Controller": [
         {
+            Title: "Web controller for Spotify",
             Description:
                 "A web application that allows users to control their Spotify playback. It utilizes the Spotify Developer API for managing playback, with user authentication handled via the OAuth 2.0 protocol.",
             GitHub: "https://github.com/otsosaarinen/spotify-web-controller",
             Badges: [
                 "TypeScript",
                 "Spotify API",
+                "REST API",
                 "OAuth 2.0",
                 "React",
                 "Tailwind CSS",
@@ -68,8 +73,9 @@ const projects: ProjectType = {
     ],
     "9ways": [
         {
+            Title: "Real-time transportation application",
             Description:
-                "Developed a transportation application for Abuja, Nigeria as a part of Turku UAS Innovation project. Website tracks the local buses and timetables. User can also search for routes through the website.",
+                "Developed a prototype transportation application for Abuja, Nigeria as a part of Turku UAS Innovation project. Website tracks the local buses in real-time, allows users to view timetables and search for routes.",
             GitHub: "https://github.com/otsosaarinen/9ways",
             Badges: [
                 "Leaflet",
@@ -98,6 +104,9 @@ export default function Projects() {
                             >
                                 <div className="text-2xl font-semibold bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-transparent">
                                     {projectKey}
+                                </div>
+                                <div className="text-neutral-500 italic">
+                                    {projectArray[0].Title}
                                 </div>
                                 <div className="flex flex-row flex-wrap gap-2">
                                     {projectArray[0].Badges.map(
